f65ad4a4339de03360d9b00e0948c644201d5de5
Esteban Palacio <estipalacio@gmail.com>
2021-12-12 11:32:55 -0500

    basic setup/structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+// styles
+import './Navbar.css'
+
+export default function Navbar() {
+    return (
+        <div className='navbar'>
+            <nav>
+                <Link to='/' className='brand'>
+                    <h1>Cooking Ninjas</h1>
+                </Link>
+                <Link to='/create'>
+                    Create Recipe
+                </Link>
+            </nav>
+        </div>
+    )
+}
---
+++ src/pages/home/Home.js
@@ -0,0 +1,17 @@
+import { useFetch } from '../../hooks/useFetch'
+
+// styles
+import './Home.css'
+
+export default function Home() {
+    const { data, isPending, error } = useFetch('http://localhost:3000/recipes ')
+    return (
+        <div className='home'>
+            {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>Loading...</p>}
+            {data && data.map(recipe => (
+                <h2 key={recipe.id}>{recipe.title}</h2>
+            ))}
+        </div>
+    )
+}
